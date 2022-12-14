a2ba385d2782be277cdf478dcad5d114c520daa2
Engin Karataş <enginkaratas99@gmail.com>
2022-12-14 20:53:43 +0300

    Updated for medium

--- computedVsFunction/app.js
+++ computedVsFunction/app.js
@@ -1,26 +1,25 @@
 let app = Vue.createApp({
   data() {
-      return {
-        a: 0,
-        b: 0
-      }
+    return {
+      a: 0,
+      b: 0,
+    };
+  },
+  computed: {
+    computedPrintText() {
+      console.log("computed printTextComputed", this.a);
     },
-    computed: {
-      computedPrintText() { 
-        console.log("computed printTextComputed", this.a);
-      },
-      computedStringifySomething(){
-        console.log("computed stringifySomething", this.b)
-      }
+    computedStringifySomething() {
+      console.log("computed stringifySomething", this.b);
     },
-    methods: {
-      printTextMethodA() {
-        console.log(" from method A:", this.a);
-      },
-      printTextMethodB() {
-        console.log(" from method B:", this.b);
-      }
+  },
+  methods: {
+    printTextMethodA() {
+      console.log(" from method A:");
     },
+    printTextMethodB() {
+      console.log(" from method B:");
+    },
+  },
 });
-
 app.mount("#assignment");
