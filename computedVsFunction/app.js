bf0a50c87fa8b6c0a3203a44bacc547411e50f57
Engin Karatas <engin.karatas@huawei.com>
2022-12-14 10:30:24 +0300

    Add Methods and computed propert examples

---
+++ computedVsFunction/app.js
@@ -0,0 +1,26 @@
+let app = Vue.createApp({
+  data() {
+      return {
+        a: 0,
+        b: 0
+      }
+    },
+    computed: {
+      computedPrintText() { 
+        console.log("computed printTextComputed", this.a);
+      },
+      computedStringifySomething(){
+        console.log("computed stringifySomething", this.b)
+      }
+    },
+    methods: {
+      printTextMethodA() {
+        console.log(" from method A:", this.a);
+      },
+      printTextMethodB() {
+        console.log(" from method B:", this.b);
+      }
+    },
+});
+
+app.mount("#assignment");
--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -4,7 +4,7 @@ defineProps({
     type: String,
     required: true
   }
-})
+}),
 </script>
 
 <template>
@@ -14,6 +14,7 @@ defineProps({
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      <p @click="greetings()"></p>
     </h3>
   </div>
 </template>
